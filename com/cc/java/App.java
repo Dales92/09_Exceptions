a4443b172c6ebdb9c4cbe0818fac33eb1aa61b9c
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-04-28 08:13:52 +0200

    Add files via upload

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,16 +1,29 @@
 package com.cc.java;
 
 public class App {
-    
-    public static void main(String[] args) {
-     
-    }
 
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
-    }
-
-
+	public static void main(String[] args) {
+		
+		String str;  // String
+		char ch; // Character
+		
+		try 
+		{		
+			str = "Irgend eine Zeichenkette";
+			System.out.println("Stringlänge :" + str.length());
+			
+			//ch = str.charAt(200);
+			//System.out.println("Character :" + ch);	
+		} 
+		catch (StringIndexOutOfBoundsException e) {
+			// Fehlerbehandlung ...
+			System.out.println("Index Fehler, bitte Eingabe prüfen!");		
+		}
+		catch (Exception e) {
+			// Weitere Fehler-Routinen ....
+		}
+		
+		
+	}
 
 }
-
